73838f8cb32047881780bab4ac5f5abf922a6dc9
Harman Singh <h286sing@uwaterloo.ca>
2020-09-04 02:04:08 -0600

    first commit

---
+++ TwitterSentimentAnalysis/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  TwitterSentimentAnalysis
+//
+//  Created by Harman Singh on 04/09/20.
+//  Copyright © 2020 Harman Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var backgroundView: UIView!
+    
+    @IBOutlet weak var emojiLabel: UILabel!
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func predictButtonPressed(_ sender: UIButton) {
+    }
+    
+
+}
+
