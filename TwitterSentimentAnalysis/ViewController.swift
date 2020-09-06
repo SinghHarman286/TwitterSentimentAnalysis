6cf0595becd62403ea2d9da9e46ef84acc3b7951
Harman Singh <h286sing@uwaterloo.ca>
2020-09-05 18:09:15 -0600

    updated UserInterface and linked the sentimentScore to the emoji displayed

--- TwitterSentimentAnalysis/ViewController.swift
+++ TwitterSentimentAnalysis/ViewController.swift
@@ -28,25 +28,62 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-                
-        swifter.searchTweet(using: "@Apple",lang: "en", count: 100, success: { (results, metadata) in
-            var tweets = [String]()
-            
-            for i in 0..<100 {
-                if let tweet = results[i]["full_text"].string {
-                    print("MAN!! the tweet shoudld be \(tweet)")
-                    tweets.append(tweet)
-                }
-            }
-            
-            print(tweets)
-        }) { (error) in
-            print("There was an error with the API request \(error)")
-        }
-        
     }
     
     @IBAction func predictButtonPressed(_ sender: UIButton) {
+        
+        if let searchText = textField.text {
+            swifter.searchTweet(using: searchText ,lang: "en", count: 100, success: { (results, metadata) in
+                var tweets = [TweetSentimentClassifierInput]()
+                
+                for i in 0..<100 {
+                    if let tweet = results[i]["full_text"].string {
+                        let tweetForClassification = TweetSentimentClassifierInput(text: tweet)
+                        tweets.append(tweetForClassification)
+                    }
+                }
+                
+                do {
+                    let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
+                    
+                    var sentimentScore = 0
+                    
+                    for prediction in predictions {
+                        let sentiment = prediction.label
+                        
+                        if sentiment == "Pos" {
+                            sentimentScore += 1
+                        }
+                        else if sentiment == "Neg" {
+                            sentimentScore -= 1
+                        }
+                    }
+                    
+                    if sentimentScore > 20 {
+                        self.emojiLabel.text = "🥰"
+                    } else if sentimentScore > 10 {
+                        self.emojiLabel.text = "😀"
+                    } else if sentimentScore > 0 {
+                        self.emojiLabel.text = "😌"
+                    } else if sentimentScore > -10 {
+                        self.emojiLabel.text = "😐"
+                    } else if sentimentScore > -20 {
+                        self.emojiLabel.text = "😡"
+                    } else {
+                        self.emojiLabel.text = "🤬"
+                    }
+                } catch {
+                    print("There was an error, \(error)")
+                }
+                
+                
+            }) { (error) in
+                print("There was an error with the API request \(error)")
+            }
+            
+        }
+        
+        
     }
     
 
